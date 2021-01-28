286a780d0eda9540f45eba19fb415cfdb39284eb
menshiva <menshiva@fit.cvut.cz>
2021-01-28 17:57:37 +0100

    Project misc
    Make forbes.asm more readable
    Remove forbes.aps
    Improve documentation
    Fix typos
    Fix whitespaces

--- src/forbes.asm
+++ src/forbes.asm
@@ -1,357 +1,389 @@
+;************************************************************************
+;
+;				  Forbes
+;
+;************************************************************************
+
+
+;---------------------------------
+; Definition file for ATmega169
 .include "m169def.inc"
 
+;---------------------------------
+; Display workflow
 .org 0x1000
 .include "utils\print.inc"
 
 .cseg
 
-.def displayChar = R16
-.def displayCharPos = R17
-.def seed = R18
-.def randNum = R19
-.def pressedButton = R20
-.def buttonLocker = R21
-.def mode = R22
-.def lastChar = R23
-.def gameResult = R24
-.def tmp = R25
+.def displayChar 	= R16	; Stores char to show on display
+.def displayCharPos 	= R17	; Position of char on display
+.def seed 		= R18	; Seed for random function
+.def randNum 		= R19	; Random number
+.def pressedButton 	= R20	; Pressed button id
+.def buttonLocker 	= R21	; "Boolean" locker for preventing button signal on hold
+.def mode 		= R22	; Gamemode
+.def lastChar 		= R23	; Last char showed on display
+.def gameResult 	= R24	; The result of the game
+.def tmp 		= R25
 
 .org 0x0000
- 	RJMP start
+	RJMP	start
 
 .org 0x100
 	; 0 HARD, 1 EASY
 	gamemode: .db 0
 
+;---------------------------------
+; Main logic
 start:
-    ; Stack init
-	LDI displayChar, 0xFF
-	OUT SPL, displayChar
-	LDI displayChar, 0x04
-	OUT SPH, displayChar
+	; Stack init
+	LDI	displayChar, 0xFF
+	OUT	SPL, displayChar
+	LDI	displayChar, 0x04
+	OUT	SPH, displayChar
 
-    ; Display init
-	CALL displayInit
+	; Display init
+	CALL	displayInit
 	; Joystick init
-	CALL init_joy
+	CALL	init_joy
 
 	; Save gamemode to mode
-	LDI R30, low(2 * gamemode)
-	LDI R31, high(2 * gamemode)
-	LPM mode, Z
+	LDI	R30, low(2 * gamemode)
+	LDI	R31, high(2 * gamemode)
+	LPM	mode, Z
 
 	; Print 'Start' on display
-	CALL clearDisplay
-	CALL print_start
-	LDI buttonLocker, 1
+	CALL	clearDisplay
+	CALL	print_start
+	LDI	buttonLocker, 1
 
-	start_loop:
-		INC seed
-		; Read center button status
-		CALL read_joy
-    	CPI pressedButton, 1
-		; If center button is pressed
-    	BRNE startCenterBtnNotPressed
-		; If buttonLocker == 0 then buttonLocker = 1 and break loop
-		; Else continue loop
-			CPI buttonLocker, 0
-			BRNE start_loop
-			INC buttonLocker
-			RJMP endStartCheckButton
-			; If center button is not pressed
-		startCenterBtnNotPressed:
-			; If buttonLocker == 1 then buttonLocker = 0
-			; Else continue loop
-			CPI buttonLocker, 1
-			BRNE start_loop
-			DEC buttonLocker
-			RJMP start_loop
+;---------------------------------
+; Waits for user to press button
+; to start a new game
+start_loop:
+	INC	seed
+	; Read center button status
+	CALL	read_joy
+	CPI	pressedButton, 1
+	; If center button is pressed
+	BRNE	startCenterBtnNotPressed
+	; If buttonLocker == 0 then buttonLocker = 1 and break loop
+	; Else continue loop
+	CPI	buttonLocker, 0
+	BRNE	start_loop
+	INC	buttonLocker
+	RJMP	endStartCheckButton
+; If center button is not pressed
+startCenterBtnNotPressed:
+	; If buttonLocker == 1 then buttonLocker = 0
+	; Else continue loop
+	CPI	buttonLocker, 1
+	BRNE	start_loop
+	DEC	buttonLocker
+	RJMP	start_loop
+endStartCheckButton:
+	CALL 	clearDisplay
+	CALL 	forbes_diplay_init
+	LDI 	displayCharPos, 6
 
-	endStartCheckButton:
+;---------------------------------
+; Rolling current slot
+roll_loop:
+	INC 	seed
+	CALL 	getRandFromSeed ; Store random number in randNum
+	MOV 	displayChar, randNum
+	CALL 	showChar
 
-	CALL clearDisplay
-	CALL forbes_diplay_init
-	LDI displayCharPos, 6
+	; Check if button pressed and wait for time
+	; Time is longer when gamemode is 1
+	LDI 	R26, 0x10
+	CPI 	mode, 1
+	BRNE 	wait
+	SUBI 	R26, -0xEF
+wait:
+	; Read center button status
+	CALL 	read_joy
+	CPI 	pressedButton, 1
+	; If center button is pressed
+	BRNE 	centerBtnNotPressed
+	; If buttonLocker == 0 then buttonLocker = 1 and displayCharPos -= 1 to set next roll
+	; Else continue loop
+	CPI 	buttonLocker, 0
+	BRNE 	endCheckButton
+	INC 	buttonLocker
 
-	roll_loop:
-		INC seed
-		CALL getRandFromSeed ; Store random number in randNum
-		MOV displayChar, randNum
-		CALL showChar
+	; Compare last char with current.
+	; If they are the same - set gameResult to 1
+	; Else set gameResult to 0.
+	; And copy current char to lastChar
+	CP 	displayChar, lastChar
+	BRNE 	setZeroResult
+	LDI 	gameResult, 1
+	RJMP 	endSetResult
+setZeroResult:
+	LDI 	gameResult, 0
+endSetResult:
+	MOV 	lastChar, displayChar
+	DEC 	displayCharPos
+	; If displayCharPos == 3 then break loop
+	CPI 	displayCharPos, 3
+	BREQ 	printResult
+	RJMP 	roll_loop
+; If center button is not pressed
+centerBtnNotPressed:
+	; If buttonLocker == 1 then buttonLocker = 0
+	; Else continue loop
+	CPI 	buttonLocker, 1
+	BRNE 	endCheckButton
+	DEC 	buttonLocker
+endCheckButton:
+	DEC 	R26
+	BRNE 	wait
+	RJMP 	roll_loop
 
-		; Check if button pressed and wait for time
-		; Time is longer when gamemode is 1
-		LDI R26, 0x10
-		CPI mode, 1
-		BRNE wait
-		SUBI R26, -0xEF
-		wait:
-			; Read center button status
-			CALL read_joy
-    		CPI pressedButton, 1
-			; If center button is pressed
-    		BRNE centerBtnNotPressed
-			; If buttonLocker == 0 then buttonLocker = 1 and displayCharPos -= 1 to set next roll
-			; Else continue loop
-				CPI buttonLocker, 0
-				BRNE endCheckButton
-				INC buttonLocker
+;---------------------------------
+; Print game result
+;
+; If gameResult == 1 then blink_winner() else blink_loser()
+printResult:
+	CPI 	gameResult, 1
+	BREQ 	win
+	CALL 	blink_loser
+	RJMP 	final_loop
+win:
+	CALL 	blink_winner
 
-				; Compare last char with current.
-				; If they are the same - set gameResult to 1
-				; Else set gameResult to 0.
-				; And copy current char to lastChar
-				CP displayChar, lastChar
-				BRNE setZeroResult
-				LDI gameResult, 1
-				RJMP endSetResult
-				setZeroResult:
-				LDI gameResult, 0
-				endSetResult:
-				MOV lastChar, displayChar
+;---------------------------------
+; Waits for user to press button
+; to accept game result
+final_loop:
+	; Read center button status
+	CALL 	read_joy
+   	CPI 	pressedButton, 1
+	; If center button is pressed
+   	BRNE 	finalCenterBtnNotPressed
+	; If buttonLocker == 0 then buttonLocker = 1 and goto start
+	; Else continue loop
+	CPI 	buttonLocker, 0
+	BRNE 	start_loop
+	INC 	buttonLocker
+	RJMP 	start
+; If center button is not pressed
+finalCenterBtnNotPressed:
+	; If buttonLocker == 1 then buttonLocker = 0
+	; Else continue loop
+	CPI 	buttonLocker, 1
+	BRNE 	final_loop
+	DEC 	buttonLocker
+	RJMP 	final_loop
 
-				DEC displayCharPos
-				; If displayCharPos == 3 then break loop
-				CPI displayCharPos, 3
-				BREQ printResult
-
-				RJMP roll_loop
-			; If center button is not pressed
-			centerBtnNotPressed:
-				; If buttonLocker == 1 then buttonLocker = 0
-				; Else continue loop
-				CPI buttonLocker, 1
-				BRNE endCheckButton
-				DEC buttonLocker
-
-			endCheckButton:
-
-		DEC R26
-		BRNE wait
-
-		RJMP roll_loop
-
-	; If gameResult == 1 then blink_winner() else blink_loser()
-	printResult:
-		CPI gameResult, 1
-		BREQ win
-			CALL blink_loser
-			RJMP final_loop
-		win:
-			CALL blink_winner
-		
-	final_loop:
-		; Read center button status
-		CALL read_joy
-    	CPI pressedButton, 1
-		; If center button is pressed
-    	BRNE finalCenterBtnNotPressed
-		; If buttonLocker == 0 then buttonLocker = 1 and goto start
-		; Else continue loop
-			CPI buttonLocker, 0
-			BRNE start_loop
-			INC buttonLocker
-			RJMP start
-			; If center button is not pressed
-		finalCenterBtnNotPressed:
-			; If buttonLocker == 1 then buttonLocker = 0
-			; Else continue loop
-			CPI buttonLocker, 1
-			BRNE final_loop
-			DEC buttonLocker
-			RJMP final_loop
-
+;---------------------------------
+; Infinite loop
 end:
-	; Infinite loop
-	RJMP end
+	RJMP 	end
 
-; Generate random number
+;---------------------------------
+; Generate a random number
 getRandFromSeed:
-	ADD randNum, seed
-	SWAP seed
+	ADD 	randNum, seed
+	SWAP 	seed
 	CLC
-	; while (randNum > 15) randNum -= 16
-	normalizeRandomNumber:
-		CPI randNum,16
-   		BRLO finishNormalize
-		SUBI randNum, 16
-		RJMP normalizeRandomNumber
-	; Convert randNum to char
-	finishNormalize:
-		SUBI randNum, -'0'
-		; If randNum is < ('9' + 1), then goto first_skip.
-		; Else randNum += ('A' - ('9' + 1)) (to get 'A', 'B', 'C', etc.)
-		CPI randNum, 58
-		BRLO endConvertingChar
-		SUBI randNum, -7
-	endConvertingChar:
+; while (randNum > 15) randNum -= 16
+normalizeRandomNumber:
+	CPI 	randNum,16
+   	BRLO 	finishNormalize
+	SUBI 	randNum, 16
+	RJMP 	normalizeRandomNumber
+; Convert randNum to char
+finishNormalize:
+	SUBI 	randNum, -'0'
+	; If randNum is < ('9' + 1), then goto first_skip.
+	; Else randNum += ('A' - ('9' + 1)) (to get 'A', 'B', 'C', etc.)
+	CPI 	randNum, 58
+	BRLO 	endConvertingChar
+	SUBI 	randNum, -7
+endConvertingChar:
 	RET
 
+;---------------------------------
 ; Blink 'WINNER' 3 times
 blink_winner:
-	LDI tmp, 0
-	blinkWin:
-		CALL clearDisplay
-		CALL waitHalfSec
-		CALL print_winner
-		CALL waitHalfSec
-		INC tmp
-		CPI tmp, 3
-		BRNE blinkWin
+	LDI 	tmp, 0
+blinkWin:
+	CALL 	clearDisplay
+	CALL 	waitHalfSec
+	CALL 	print_winner
+	CALL 	waitHalfSec
+	INC	tmp
+	CPI 	tmp, 3
+	BRNE 	blinkWin
 	RET
 
+;---------------------------------
 ; Blink 'LOSER' 3 times
 blink_loser:
-	LDI tmp, 0
-	blinkLose:
-		CALL clearDisplay
-		CALL waitHalfSec
-		CALL print_loser
-		CALL waitHalfSec
-		INC tmp
-		CPI tmp, 3
-		BRNE blinkLose
+	LDI 	tmp, 0
+blinkLose:
+	CALL 	clearDisplay
+	CALL 	waitHalfSec
+	CALL 	print_loser
+	CALL 	waitHalfSec
+	INC 	tmp
+	CPI 	tmp, 3
+	BRNE 	blinkLose
 	RET
 
-; Function to print 'START' on display
+;---------------------------------
+; Print 'START' on display
 print_start:
-	LDI displayCharPos, 2
-	LDI displayChar, 'S'
-	CALL showChar
-	INC displayCharPos
-	LDI displayChar, 'T'
-	CALL showChar
-	INC displayCharPos
-	LDI displayChar, 'A'
-	CALL showChar
-	INC displayCharPos
-	LDI displayChar, 'R'
-	CALL showChar
-	INC displayCharPos
-	LDI displayChar, 'T'
-	CALL showChar
+	LDI 	displayCharPos, 2
+	LDI 	displayChar, 'S'
+	CALL 	showChar
+	INC 	displayCharPos
+	LDI 	displayChar, 'T'
+	CALL 	showChar
+	INC 	displayCharPos
+	LDI 	displayChar, 'A'
+	CALL 	showChar
+	INC 	displayCharPos
+	LDI 	displayChar, 'R'
+	CALL 	showChar
+	INC 	displayCharPos
+	LDI 	displayChar, 'T'
+	CALL 	showChar
 	RET
 
-; Function to print 'WINNER' on display
+;---------------------------------
+; Print 'WINNER' on display
 print_winner:
-	LDI displayCharPos, 2
-	LDI displayChar, 'W'
-	CALL showChar
-	INC displayCharPos
-	LDI displayChar, 'I'
-	CALL showChar
-	INC displayCharPos
-	LDI displayChar, 'N'
-	CALL showChar
-	INC displayCharPos
-	LDI displayChar, 'N'
-	CALL showChar
-	INC displayCharPos
-	LDI displayChar, 'E'
-	CALL showChar
-	INC displayCharPos
-	LDI displayChar, 'R'
-	CALL showChar
+	LDI 	displayCharPos, 2
+	LDI 	displayChar, 'W'
+	CALL 	showChar
+	INC 	displayCharPos
+	LDI 	displayChar, 'I'
+	CALL 	showChar
+	INC 	displayCharPos
+	LDI 	displayChar, 'N'
+	CALL 	showChar
+	INC 	displayCharPos
+	LDI 	displayChar, 'N'
+	CALL 	showChar
+	INC 	displayCharPos
+	LDI 	displayChar, 'E'
+	CALL 	showChar
+	INC 	displayCharPos
+	LDI 	displayChar, 'R'
+	CALL 	showChar
 	RET
 
-; Function to print 'LOSER' on display
+;---------------------------------
+; Print 'LOSER' on display
 print_loser:
-	LDI displayCharPos, 2
-	LDI displayChar, 'L'
-	CALL showChar
-	INC displayCharPos
-	LDI displayChar, 'O'
-	CALL showChar
-	INC displayCharPos
-	LDI displayChar, 'S'
-	CALL showChar
-	INC displayCharPos
-	LDI displayChar, 'E'
-	CALL showChar
-	INC displayCharPos
-	LDI displayChar, 'R'
-	CALL showChar
+	LDI 	displayCharPos, 2
+	LDI 	displayChar, 'L'
+	CALL 	showChar
+	INC 	displayCharPos
+	LDI 	displayChar, 'O'
+	CALL 	showChar
+	INC 	displayCharPos
+	LDI 	displayChar, 'S'
+	CALL 	showChar
+	INC 	displayCharPos
+	LDI 	displayChar, 'E'
+	CALL 	showChar
+	INC 	displayCharPos
+	LDI 	displayChar, 'R'
+	CALL 	showChar
 	RET
 
-; Function to print start rolls positions on display (0 0 0)
+;---------------------------------
+; Print rolls initial values
+; (0 0 0)
 forbes_diplay_init:
-	LDI displayCharPos, 4
-	LDI displayChar, '0'
-	CALL showChar
-	INC displayCharPos
-	LDI displayChar, '0'
-	CALL showChar
-	INC displayCharPos
-	LDI displayChar, '0'
-	CALL showChar
+	LDI 	displayCharPos, 4
+	LDI 	displayChar, '0'
+	CALL 	showChar
+	INC 	displayCharPos
+	LDI 	displayChar, '0'
+	CALL 	showChar
+	INC 	displayCharPos
+	LDI 	displayChar, '0'
+	CALL 	showChar
 	RET
 
-; Function to clear display.
-; for (int i = 2; i < 8; ++i) print(' ')
+;---------------------------------
+; Clear display
 clearDisplay:
-	LDI displayCharPos, 2
-	LDI displayChar, ' '
-	startClear:
-		CALL showChar
-		INC displayCharPos
-		CPI displayCharPos, 8
-		BREQ endClear
-		RJMP startClear
-	endClear:
+	LDI 	displayCharPos, 2
+	LDI 	displayChar, ' '
+startClear:
+	CALL 	showChar
+	INC 	displayCharPos
+	CPI 	displayCharPos, 8
+	BREQ 	endClear
+	RJMP 	startClear
+endClear:
 	RET
 
-; Joystick init function
+;---------------------------------
+; Joystick initialization
 init_joy:
-    IN R17, DDRE
-    ANDI R17, 0b11110011
-    IN R16, PORTE
-    ORI R16, 0b00001100
-    OUT DDRE, R17
-    OUT PORTE, R16
-    LDI R16, 0b00000000
-    STS DIDR1, R16
-    IN R17, DDRB
-    ANDI R17, 0b00101111
-    IN R16, PORTB
-    ORI R16, 0b11010000
-    OUT DDRB, R17
-    OUT PORTB, R16
+	IN 	R17, DDRE
+	ANDI 	R17, 0xF3
+	IN 	R16, PORTE
+	ORI 	R16, 0xC
+	OUT 	DDRE, R17
+	OUT 	PORTE, R16
+	LDI 	R16, 0x0
+	STS 	DIDR1, R16
+	IN 	R17, DDRB
+	ANDI 	R17, 0x2F
+	IN 	R16, PORTB
+	ORI 	R16, 0xD0
+	OUT 	DDRB, R17
+	OUT 	PORTB, R16
 	RET
 
-; Put joystick pressed button to R20
+;---------------------------------
+; Store joystick pressed button
+; in R20
 read_joy:
-    PUSH R16
-    PUSH R17
-	joy_reread:
-    	IN R16, PINB
-    	LDI pressedButton, 255
-	joy_wait:
-		DEC pressedButton
-    	BRNE joy_wait
-    	IN R17, PINB
-    	ANDI R16, 0b00010000
-    	ANDI R17, 0b00010000
-    	CP R16, R17
-    	BRNE joy_reread
-    	LDI R20, 0
-    	CPI R16, 0
-    	BRNE joy_no_enter
-    	LDI pressedButton, 1
-	joy_no_enter:
-		POP R17
-		POP R16
+	PUSH 	R16
+	PUSH 	R17
+joy_reread:
+   	IN 	R16, PINB
+   	LDI 	pressedButton, 255
+joy_wait:
+	DEC 	pressedButton
+   	BRNE 	joy_wait
+   	IN 	R17, PINB
+   	ANDI 	R16, 0x10
+   	ANDI 	R17, 0x10
+   	CP 	R16, R17
+   	BRNE 	joy_reread
+   	LDI 	R20, 0
+   	CPI 	R16, 0
+   	BRNE 	joy_no_enter
+   	LDI 	pressedButton, 1
+joy_no_enter:
+	POP 	R17
+	POP 	R16
 	RET
 
-; Function to sleep for 500 ms.
+;---------------------------------
+; Sleep for 500 ms.
+;
 ; CPU frequency = 2 MHz = 2*10^6 ticks.
 ; 2*10^6 ticks ~ 3 ticks * 10 * 255 * 255 instructions.
 waitHalfSec:
-	LDI R28, 0xFF
-	waitHalf2:
-		LDI R27, 0xFF
-			waitHalf:
-				DEC R27
-	   			BRNE waitHalf
-		DEC R28
-		BRNE waitHalf2
+	LDI 	R28, 0xFF
+waitHalf2:
+	LDI 	R27, 0xFF
+	waitHalf:
+	DEC 	R27
+	BRNE 	waitHalf
+	DEC 	R28
+	BRNE 	waitHalf2
 	RET
+
